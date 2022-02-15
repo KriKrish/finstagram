eaf5627a75b0827cfa8e777617c5000abf489d12
Krithi <krithi.vlsi@gmail.com>
2022-02-15 00:35:02 +0000

    W5D1

--- app/actions.rb
+++ app/actions.rb
@@ -6,7 +6,7 @@ get '/' do
   get '/signup' do     # if a user navigates to the path "/signup",
     @user = User.new   # setup empty @user object
     erb(:signup)       # render "app/views/signup.erb"
-  end
+  end  
 
   post '/signup' do
     email      = params[:email]
@@ -20,5 +20,34 @@ get '/' do
       "User #{username} saved!"
     else
       erb(:signup)
+    end
+  end
+
+  get '/login' do    # when a GET request comes into /login
+    #erb(:login)      # render app/views/login.erb
+    params.to_s
+  end
+
+  post '/login' do
+    username = params[:username]
+    password = params[:password]
+  
+    # 1. find user by username
+    user = User.find_by(username: username)
+  
+    # 2. if that user exists
+    if user
+  
+      # check if that user's password matches the password input
+      # 3. if the passwords match
+      if user.password == password
+  
+        # login (more to come here)
+        "Success!"
+      else
+        "Password doesn't match"
+      end
+    else
+      "No user"
     end
   end
