7e3890f2b483faaed543f3984ee46d29a9f3290b
Krithi <krithi.vlsi@gmail.com>
2022-02-17 01:21:03 +0000

    W5D2

--- app/actions.rb
+++ app/actions.rb
@@ -1,3 +1,9 @@
+helpers do
+  def current_user
+    User.find_by(id: session[:user_id])
+  end
+end
+
 get '/' do
     @finstagram_posts = FinstagramPost.order(created_at: :desc)
     #@current_user = User.find_by(id: session[:user_id])
@@ -53,8 +59,28 @@ get '/' do
     redirect to('/')
   end
 
-  helpers do
-    def current_user
-      User.find_by(id: session[:user_id])
+  
+
+  get '/finstagram_posts/new' do
+    @finstagram_post = FinstagramPost.new
+    erb(:"finstagram_posts/new")
+  end
+  
+  post '/finstagram_posts' do
+    photo_url = params[:photo_url]
+  
+    @finstagram_post = FinstagramPost.new({ photo_url: photo_url, user_id: current_user.id })
+  
+    if @finstagram_post.save
+      redirect(to('/'))
+    else
+      erb(:"finstagram_posts/new")
     end
+  end
+
+  get '/finstagram_posts/:id' do
+    #params[:id]
+    @finstagram_post = FinstagramPost.find(params[:id])   # find the finstagram post with the ID from the URL
+    #escape_html @finstagram_post.inspect        # print to the screen for now
+    erb(:"finstagram_posts/show")               # render app/views/finstagram_posts/show.erb
   end
